159612959415c08378733b0056cb1f62feffe5a6
Stephen T <xxlennygamerxx@gmail.com>
2024-10-28 03:02:19 -0500

    Annoying update to DepotDownloader I'm not bothering with.

---
+++ src/Reaganism.Paperclip/Platform/ReducedPlatform.cs
@@ -0,0 +1,18 @@
+using DepotDownloader.Platform;
+
+namespace Reaganism.Paperclip.Platform;
+
+/// <summary>
+///     A reduced platform implementation that wraps around a real
+///     <see cref="IPlatform"/> implementation and avoids unnecessary operations
+///     in the context of paperclip.
+/// </summary>
+/// <param name="platform">The platform to wrap.</param>
+internal sealed class ReducedPlatform(IPlatform platform) : IPlatform
+{
+    void IPlatform.SetExecutable(string path, bool value)
+    {
+        // Don't do anything in the context of paperclip.  We don't need
+        // executables to actually be executable.
+    }
+}
